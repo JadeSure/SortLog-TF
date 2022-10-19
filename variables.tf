81266a3f92121420fa6444576f9df0dc305aa528
shawn Wong <jadesure17@gmail.com>
2022-10-19 14:23:32 +1100

    s3 + cdn no problem

--- variables.tf
+++ variables.tf
@@ -11,11 +11,4 @@ variable "cdn_comment" {
   type = string
 }
 
-variable "vpc_cidr_block" {}
-variable "subnet_cidr_block" {}
-variable "avail_zone" {}
-variable "env_prefix" {}
 variable "region" {}
-variable "my_ip_address" {}
-variable "instance_type" {}
-variable "public_key_location" {}
