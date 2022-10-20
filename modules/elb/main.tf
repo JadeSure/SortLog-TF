2d0c56a96bb0d4159b2c644d24df8c8a91d3a0fa
shawn Wong <jadesure17@gmail.com>
2022-10-20 22:56:48 +1100

    prepare alb

---
+++ modules/elb/main.tf
@@ -0,0 +1,54 @@
+resource "aws_lb" "alb_backend" {
+  name               = "ECS-ALB"
+  internal           = false
+  load_balancer_type = "application"
+  security_groups    = [aws_security_group.lb_sg.id]
+  subnets            = [for subnet in aws_subnet.public : subnet.id]
+
+  enable_deletion_protection = true
+
+  access_logs {
+    bucket  = aws_s3_bucket.lb_logs.bucket
+    prefix  = "test-lb"
+    enabled = true
+  }
+
+  tags = {
+    Environment = "production"
+  }
+}
+
+
+data "aws_acm_certificate" "issued" {
+  domain   = "tf.example.com"
+  statuses = ["ISSUED"]
+}
+
+
+resource "aws_lb_target_group" "alb-example" {
+  name        = "tf-example-lb-alb-tg"
+  target_type = "alb"
+  port        = 80
+  protocol    = "TCP"
+  vpc_id      = aws_vpc.main.id
+
+  health_check {
+    
+  }
+}
+
+
+
+resource "aws_lb_listener" "front_end" {
+  load_balancer_arn = aws_lb.front_end.arn
+  port              = "443"
+  protocol          = "HTTPS"
+  ssl_policy        = "ELBSecurityPolicy-2016-08"
+  certificate_arn   = "arn:aws:iam::187416307283:server-certificate/test_cert_rab3wuqwgja25ct3n4jdj2tzu4"
+
+  default_action {
+    type             = "forward"
+    target_group_arn = aws_lb_target_group.front_end.arn
+  }
+}
+
